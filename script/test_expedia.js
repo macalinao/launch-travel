14aa14ef3dd1bb4933f8f2e99957f15c5559f72e
Ian Macalinao <me@ian.pw>
2016-02-27 15:24:40 -0800

    add long description

--- script/test_expedia.js
+++ script/test_expedia.js
@@ -3,7 +3,7 @@ const expedia = require('../lib/expedia');
 expedia.
   cityInfo('San Francisco').
   then(result => {
-    console.log(JSON.stringify(result));
+    console.log(JSON.stringify(result, null, 2));
   }).
   catch(error => {
     console.error(error.message);
