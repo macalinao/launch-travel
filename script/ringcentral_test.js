41a58938cd33eb3bea58051894e926eacfef89d0
Ian Macalinao <me@ian.pw>
2016-02-27 23:47:17 -0800

    lol

--- script/ringcentral_test.js
+++ script/ringcentral_test.js
@@ -1,12 +1,27 @@
 const ringcentral = require('../lib/ringcentral');
+const data = require('../data/expedia.json');
+const hotel = data.hotel;
+
+const NUMBER = '+15107368436';
 
 ringcentral.login().
-  then(result => {
-    // return ringcentral.sms('+15107368436', 'Fares changed from $123.43 to $110.25. Act now!');
-    return ringcentral.messages();
+  then(() => {
+    return ringcentral.sms(NUMBER, 'Your hotel price has dropped to $123.24 per night. Book now?');
   }).
-  then(result => {
-    console.log(result.map(x => x.subject));
+  then(() => {
+    return ringcentral.listenToSMS(function(msg) {
+      ringcentral.messages().then(msgs => {
+        console.log('if young metro dont trust you imma shoot you');
+        return msgs[0].subject;
+      }).then(txt => {
+        if (txt.startsWith('Ok, book your hotel')) return null;
+        console.log(txt);
+        return ringcentral.sms(NUMBER, `Ok, book your hotel at ${hotel.link}`);
+      }).catch(console.error);
+    });
+  }).
+  then(() => {
+    console.log('Registered SMS subscription');
   }).
   catch(error => {
     console.log(error);
